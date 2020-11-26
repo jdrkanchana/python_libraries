4f54b4be7bc5f316f38fd8d5f374c019a9a97b74
jdrkanchana <66084534+jdrkanchana@users.noreply.github.com>
2020-11-26 22:47:58 +0530

    Update iris_dataset_log_reg.py

--- numpy/iris_dataset_log_reg.py
+++ numpy/iris_dataset_log_reg.py
@@ -1,4 +1,3 @@
-
 """
 Code IDE is spyder
 Using logistic regression in scikit to iris dataset
@@ -8,9 +7,13 @@ Using logistic regression in scikit to iris dataset
 from sklearn.datasets import load_iris
 iris=load_iris()
 
-# create X(features) and y(response), X is a matrix and y is a vector
+# create X(featuures) and y(response)
 X=iris.data
 y=iris.target
+
+# dataset split ,0.6 ratio train data set, 0.4 ratio test data set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.4)
 
 # importing the logistic regression class
 from sklearn.linear_model import LogisticRegression
@@ -19,14 +22,14 @@ from sklearn.linear_model import LogisticRegression
 log_reg=LogisticRegression()
 
 # fit the model with data
-log_reg.fit(X, y) 
+log_reg.fit(X_train, y_train) 
 
 # predict the response values for the observations in X
-log_reg.predict(X)
+log_reg.predict(X_test)
 
 # store the predicted response value
-y_pred = log_reg.predict(X)
+y_pred = log_reg.predict(X_test)
 
 # now to check accuracy of the applied logistic regession class
 from sklearn import metrics
-print (metrics.accuracy_score( y, y_pred))
+print (metrics.accuracy_score( y_test, y_pred))
